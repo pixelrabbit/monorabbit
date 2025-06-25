eb867945f5c96c34f83f207ca5d052823681b4ff
pixelrabbit <dangovea@gmail.com>
2025-06-25 16:55:10 -0500

    radial bgs

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -38,16 +38,20 @@ export default function RootLayout({
       <body
         className={`${roboto.variable}  antialiased m-0`}
       >
-        <div className={`${sourGummy.className} h-90 bg-lime-500 overflow-hidden text-slate-800 pb-4`}>
+        <div className={`${sourGummy.className} h-90 overflow-hidden text-slate-800 pb-4 bg-abstract`}>
           <div className="max-w-7xl px-4 m-auto flex h-full justify-between items-end">
             <div className={`self-end`}>
               <h1 className={`text-5xl sm:text-8xl tracking-tight`}>PiXELRABBiT</h1>
-              <p>Well, he's just this developer, you know...</p>
+              <p className="sm:text-2xl">He's just this developer, you know...</p>
             </div>
-            <nav className="flex gap-8 text-2xl">
-              {Object.entries(navItems).map(([label, href]) => (
-                <>{label}</>
-              ))}
+            <nav className="text-2xl">
+              <ul className="flex gap-8">
+                {Object.entries(navItems).map(([label, href]) => (
+                  <li key={label}>
+                    <a href={href} className="px-3 py-1 transition-colors rounded-xl border text-slate-950 border-slate-950 hover:border-white hover:text-white">{label}</a>
+                  </li>
+                ))}
+              </ul>
             </nav>
           </div>
         </div>
